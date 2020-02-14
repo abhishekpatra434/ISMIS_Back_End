fc7c3fe7e9c3a37b524384010dbe84a414a1f736
Abhishek <abhishekpatra62@gmail.com>
2020-02-14 18:42:37 +0530

    updating

--- ISMIS_Backend/src/main/java/com/ISMIS/controller/UserAction.java
+++ ISMIS_Backend/src/main/java/com/ISMIS/controller/UserAction.java
@@ -41,7 +41,7 @@ public class UserAction {
 
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public String homePage(HttpServletRequest req, HttpServletResponse res) throws SQLException {
-		System.out.println("hlookj");
+		//System.out.println("hlookj");
 		return "Welcome to IS-MIS Home o ss";
 	}
 	
